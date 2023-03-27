c13661531a7b2d5e6e399df5c048858698b28d88
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-03-28 01:33:39 +0300

    Created model article and category

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $table = "articles";
+
+    protected $fillable = [
+        "title",
+        "body",
+        "is_active",
+        "category_id",
+        "slug_name",
+    ];
+    protected $guarded = [];
+    
+}
