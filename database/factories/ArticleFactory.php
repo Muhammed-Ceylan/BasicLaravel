c7b7015810b350529bf4919593b589686ebc0c98
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-03-28 01:34:09 +0300

    Created factories article and category

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->title;
+        return [
+            'title' => $title,
+            "body" => fake()->paragraph,
+            'slug_name' => Str::slug($title),
+            "is_active" => fake()->boolean,
+            "category_id" => random_int(1, 10),
+        ];
+    }
+}
